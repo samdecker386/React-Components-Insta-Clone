fa76d59b6ea931787dc67730e753a43180742242
Sam Decker <samdecker386@gmail.com>
2020-02-18 13:49:06 -0700

    Header and likes

--- src/App.js
+++ src/App.js
@@ -6,13 +6,19 @@ Look at each file to see where you need to pass props or add code
 */
 import React from "react";
 import "./App.css";
+import LikeSection from "./components/PostsContainer/LikeSection";
 // import the PostsPage and SearchBar and add them to the App
+import PostsPage from "./components/PostsContainer/PostsPage";
+import SearchBar from "./components/SearchBar/SearchBarContainer";
 
 
 const App = () => {
 
   return (
     <div className="App">
+      <PostsPage/>
+      <SearchBar/>
+      <LikeSection/>
       {/* Add imported components here to render them */}
     </div>
   );
--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -1,9 +1,12 @@
 // look at the likes on this component. Right now it is hard coded on line 20.
 // console.log your props and see how to add likes to this component so that it will update when you click the icon.
 // HINT: You will need to add an onClick event pass through your props and update the hard coded Likes
-import React from "react";
+import React, {useState} from "react";
 
 const LikeSection = props => {
+  const [likes, setLikes] = useState(0);
+  console.log("First console.log", useState());
+
   return (
     <div>
     <div
@@ -11,7 +14,7 @@ const LikeSection = props => {
       key="likes-icons-container"
     >
       <div className="like-section-wrapper">
-        <i className="far fa-heart" />
+        <i onClick={() => setLikes(likes + 1)} className="far fa-heart" />
       </div>
       <div className="like-section-wrapper">
         <i className="far fa-comment" />
@@ -19,7 +22,7 @@ const LikeSection = props => {
     </div>
     <p className="like-number">
       
-      likes</p>
+      {likes}</p>
 </div>
   )
 };
